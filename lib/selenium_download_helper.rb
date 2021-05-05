ed54421e3e80f84822b3cb4f0b45a23ccc312381
aki <lala.akira@gmail.com>
2021-05-05 10:08:00 +0900

    refactor: use after callback

--- lib/selenium_download_helper.rb
+++ lib/selenium_download_helper.rb
@@ -3,19 +3,29 @@
 require_relative "selenium_download_helper/version"
 
 module SeleniumDownloadHelper
+  extend ActiveSupport::Concern
+
+  DOWNLOAD_BASE_DIR = 'tmp/data/downloads'
+  private_constant :DOWNLOAD_BASE_DIR
+
+  included do
+    after(:all) do
+      FileUtils.rm_rf(Rails.root.join(DOWNLOAD_BASE_DIR))
+    end
+  end
+
   def wait_for_downloaded(timeout: 10, interval: 0.2, all: false, &block)
     download_path= mkdownloaddir
     page.driver.browser.download_path = download_path
     yield if block_given?
     Selenium::WebDriver::Wait.new(timeout: timeout, interval: interval).until { downloaded?(download_path) }
-    at_exit { FileUtils.rm_rf(download_path) }
     downloaded_files(download_path).then { |files| all ? files : files.first }
   end
 
   private
 
     def mkdownloaddir
-      Dir.mktmpdir('SeleniumDownloadHelper_', Rails.root.join('tmp/data/downloads'))
+      Dir.mktmpdir('SeleniumDownloadHelper_', Rails.root.join(DOWNLOAD_BASE_DIR))
     end
 
     def downloaded_files(download_path)
